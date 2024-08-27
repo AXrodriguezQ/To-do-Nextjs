0636334c790481a94665d18f7cbaeb4d15abe467
Alexander Rodriguez <160079823+AXrodriguezQ@users.noreply.github.com>
2024-08-27 17:43:26 +0200

    add routes of the tasks

---
+++ src/app/(tasks)/finished/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Tasks finished</div>
+  )
+}
+
+export default page
--- src/components/Molecules/SectionSidebarList/ListItemsMolecule.tsx
+++ src/components/Molecules/SectionSidebarList/ListItemsMolecule.tsx
@@ -3,9 +3,9 @@ import TextListAtom from '@/components/Atoms/SectionSidebarList/TextListAtom'
 import Link from 'next/link'
 import React from 'react'
 
-const ListItemsMolecule = ( { text = '', urlSvg = process.env.ICON_URL } ) => {
+const ListItemsMolecule = ( { hrefUrl = "/", text = "", urlSvg = process.env.ICON_URL } ) => {
   return (
-    <Link href="/to-do/list-task" className="flex gap-2 hover:text-blue-500 hover:translate-x-1 duration-100">
+    <Link href={hrefUrl} className="flex gap-2 hover:text-blue-500 hover:translate-x-1 duration-100">
       <SvgListAtom urlSvg={urlSvg} />
       <TextListAtom text={text} />
     </Link>
